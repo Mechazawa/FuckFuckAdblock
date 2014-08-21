3014c725045399fb106ca754474757d4793e2939
shodan <Aapje1>
2014-08-21 13:31:46 +0200

    init

---
+++ FuckFuckAdBlock.js
@@ -0,0 +1,90 @@
+// ==UserScript==
+// @name            Fuck FuckAdBlock
+// @author          Mechazawa
+// @description     Acts like FuckAdBlock.js but always says that no adblock was detected.
+// @license         WTFPl
+// @version         1
+// @include         *
+// @run-at document-start
+// @grant none
+// ==/UserScript==
+
+(function(window) {
+    var FuckAdBlock = function(options) {
+        if(options !== undefined) 
+            this.setOption(options)
+
+        var self = this;
+        window.addEventListener('load', function(){
+            setTimeout(function(){
+                if(self._options.checkOnLoad === true) {
+                    self.check(false);
+                }
+            }, 1);
+        }, false);
+    }
+
+    FuckAdBlock.prototype.setOption = function(options, value) {
+        if(value !== undefined) {
+            var key = options;
+            options = {};
+            options[key] = value;
+        }
+        for(option in options)
+            this._options[option] = options[option];
+        return this;
+    };
+
+    FuckAdBlock.prototype._options = {
+        checkOnLoad:    true,
+        resetOnEnd:     true,
+        baitClass:      '',
+        baitStyle:      '',
+    };
+
+    FuckAdBlock.prototype._var = {
+        version:    '3.0.1',
+        bait:       null,
+        checking:   false,
+        event:  {
+            notDetected: []
+        }   
+    };
+
+    FuckAdBlock.prototype.check = function(ignore) {
+        this.emitEvent(false);
+        return true;
+    }
+
+    FuckAdBlock.prototype.clearEvent = function() {
+        this._var.event.notDetected = [];
+    };
+
+    FuckAdBlock.prototype.emitEvent = function(detected) {
+        if(detected === false) {
+            var fns = this._var.event.notDetected;
+            for(i in fns)
+                fns[i]();
+
+            if(this._options.resetOnEnd === true)
+                this.clearEvent();
+        }
+        return this;
+    }
+
+    FuckAdBlock.prototype.on = function(detected, fn) {
+        if(detected === false)
+            this._var.event.notDetected.push(fn);
+        return this;
+    };
+
+    FuckAdBlock.prototype.onDetected = function(fn) {
+        return this;
+    };
+
+    FuckAdBlock.prototype.onNotDetected = function(fn) {
+        return this.on(false, fn);
+    };
+
+    window.fuckAdBlock = new FuckAdBlock();
+})(window);
