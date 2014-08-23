7fe239aa546727a65e241e8ee8377142f2ad21f7
shodan <Aapje1>
2014-08-23 21:27:39 +0200

    Version bump

--- FuckFuckAdBlock.user.js
+++ FuckFuckAdBlock.user.js
@@ -4,7 +4,7 @@
 // @namespace       Mechazawa
 // @description     Acts like FuckAdBlock.js but always says that no adblock was detected.
 // @license         WTFPl
-// @version         1
+// @version         2
 // @include         *
 // @run-at          document-start
 // @updateURL       https://raw.githubusercontent.com/Mechazawa/FuckFuckAdblock/master/FuckFuckAdBlock.user.js
@@ -18,8 +18,8 @@
             this.setOption(options);
 
         var self = this;
-        window.addEventListener('load', function(){
-            setTimeout(function(){
+        window.addEventListener('load', function() {
+            setTimeout(function() {
                 if(self._options.checkOnLoad === true)
                     self.check(false);
             }, 1);
@@ -36,7 +36,7 @@
 
             for(option in options)
                 this._options[option] = options[option];
-            
+
             return this;
         },
 
